ba2511a75b6b590f592db60e62228d3b413b7700
hankoon <galaxykhh@gmail.com>
2021-05-23 01:28:36 +0900

    add styled-reset, ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './style/global';
+import { theme } from './style/theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
         <GlobalStyle />
         <BrowserRouter>
             <Switch>
@@ -12,7 +14,7 @@ function App() {
                 </Route>
             </Switch>
         </BrowserRouter>
-    </>
+      </ThemeProvider>
   );
 }
 
---
+++ src/style/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+    anything: {
+        /* you can write anything you want */
+    },
+};
